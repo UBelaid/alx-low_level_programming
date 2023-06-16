cb5943a4a7ffe9b0ca64095dbcb2fcab0fa77264
UBelaid <Usama.o.belaid@proton.me>
2023-06-16 12:59:09 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
  * Description: program prints with 'puts' function
  *
- * return :Always 0 (Success)
+ * Return : Always 0 (Success)
  */
-int main(void){
+
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
 
 
